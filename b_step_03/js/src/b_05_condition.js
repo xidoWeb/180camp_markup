fba1bf635a4870efe54164b0a349e5ee54ebf27b
xidoweb <xidoweb@gmail.com>
2021-09-22 22:17:20 +0900

    js-반법문-for

--- b_step_03/js/src/b_05_condition.js
+++ b_step_03/js/src/b_05_condition.js
@@ -50,3 +50,70 @@ if(n >= 90){
   nResult = '조금만 더 분발하셔야 겠어요. 화이팅!!!';
 }
 console.log( nResult );
+
+console.clear();
+// -----------------------------
+// switch
+
+/*
+  switch (매개변수) {
+    case 조건1:
+      기능구현....
+    break;
+    
+    case 조건2:
+      기능 구현....
+    break;
+
+    default :
+      위기능에 해당하지 않는 기능
+  }
+*/
+
+
+var i = 2;
+var iResult;
+switch (i) {
+  case 1:
+    iResult = "숫자 1을 선택하였습니다.";
+  break;
+    
+  case 2:
+    iResult = "숫자 2을 선택하였습니다.";
+  break;
+
+  case 3:
+    iResult = "숫자 3을 선택하였습니다.";
+  break;
+
+  default:
+    iResult = "숫자 1,2,3 이외의 것을 선택하였습니다.";
+}
+console.log( iResult );
+
+console.clear();
+// -------------------------------------------
+// 버튼을 클릭시 -> 이벤트발생('click',function(){})
+// id="old" value값을 찾아오기
+// 20보다 크거나 같으면 성인으로, 작으면 미성년자로 구분하여 처리
+
+// NaN : 숫자의 개념이지만 수치로 표현이 불가능하다 라는 뜻
+
+var btn = document.getElementsByTagName('button')[0];
+var input = document.getElementById('old');
+var p = document.getElementsByTagName('p')[1];
+var span = p.getElementsByTagName('span')[0];
+
+
+btn.addEventListener('click', function(){
+  var inputData = parseInt(input.value);
+  // console.log( typeof(inputData), !!inputData );
+
+  if(inputData >= 20 && !!inputData){
+    span.innerText = '성인입니다.';
+  }else if(inputData >= 1 && !!inputData){
+    span.innerText = '미성년자 입니다.';
+  }else {
+    span.innerText = '정확한 나이를 입력하세요.'
+  }
+});
