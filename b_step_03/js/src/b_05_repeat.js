87fe6b9ea02301ed8f3ab13d59002265b3ce5140
xidoweb <xidoweb@gmail.com>
2021-09-22 22:47:26 +0900

    js - 반복문 -for in, forEach

--- b_step_03/js/src/b_05_repeat.js
+++ b_step_03/js/src/b_05_repeat.js
@@ -24,8 +24,36 @@ console.clear();
 
 console.clear();
   // ------------------------------------
-// for-in
+// for-in : 정의를 위해 만든 객체를 위한 반복문
+var drink = {
+  'coffee': '에스프레소, 카페라떼, 바닐라 마끼아또, 카페모카',
+  'juice' : '포토쥬스, 딸기쥬스, 사과쥬스',
+  'ade'   : '딸기에이드, 석류에이드, 포도에이드, 레몬에이드'
+};
 
+var o;
+// for(; o < 5; o+=1){  console.log( fruits[o] ); }
+console.log( drink );
+for(o in drink){
+  console.log( o, ' : ', drink[o] );
+}
 
+console.clear();
 // ------------------------------------
 // forEach
+var fruits = ['apple', 'banana', 'grape', 'straw berry', '키위'];
+// 배열.forEach( function(매개체, index){ 기능 } );
+
+fruits.forEach( function(data, idx){
+  // console.log(data, idx);
+  console.log( (idx + 1)+ ':' + data );
+});
+
+console.clear();
+console.log( 'fruits갯수:', fruits.length);
+for(o=0; o < fruits.length ; o+=1){
+  console.log( fruits[o] );
+}
+
+console.clear();
+// ------------------------------------
