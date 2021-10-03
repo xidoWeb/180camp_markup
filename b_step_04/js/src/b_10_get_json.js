6b41bf29e16958752c6e672cd5c76d48aa72b26e
xidoweb <xidodlab@gmail.com>
2021-10-03 21:26:26 +0900

    jquery - json을 이용하여 외부데이터로 처리 및 불러오기

--- b_step_04/js/src/b_10_get_json.js
+++ b_step_04/js/src/b_10_get_json.js
@@ -1,4 +1,5 @@
 // data폴더에 coffee_list.json 파일 불러오기
+/**
 
   var coffee_list = [  
     {
@@ -50,7 +51,7 @@
       "content" : "내용을 간단하게 작성",
       "link" : "./data_deatail/coffee_other.html"}
   ];
-
+*/
 
 // 객체 데이터를 외부로 json방식으로 내보내기 위한 조건
 // 1. 주석 사용할 수 없다.
@@ -63,43 +64,56 @@
 // b_10_get_json.js
 
 (function($){
-  
-  
-//  변수
-  
-var liEl = '<li><div class="img_space"><span class="blind"></span></div><dl><dt></dt><dd class="data_con"></dd><dd class="link"><a href="#">자세히 보기</a></dd></dl></li>';
-var exWrap = $(".ex_wrap");
-var exUl = exWrap.children("ul");
+  // 외부데이터를 불러와서 사용하기
+  // $.ajax() -> html문서를 기준으로 경로를 설정
+  /*
+  $.ajax({
+    // 외부데이터를 불러오고 그 형식과 내용을 세팅
+  }).done(function(data){
+    // 코드를 모두 담는다.
+  });
+  */
 
-var i = 0;
-var len = coffee_list.length;
-for(; i < len; i += 1){
-  exUl.append(liEl);
-};
+  $.ajax({
+    url:"../json/coffee_list.json",
+    context:document.body
+  }).done(function(data){  
+    var coffee_list = data;
+    console.log( coffee_list );
+    //  변수    
+    var liEl = '<li><div class="img_space"><span class="blind"></span></div><dl><dt></dt><dd class="data_con"></dd><dd class="link"><a href="#">자세히 보기</a></dd></dl></li>';
+    var exWrap = $(".ex_wrap");
+    var exUl = exWrap.children("ul");
 
-var exLi = exUl.children("li");
+    var i = 0;
+    var len = coffee_list.length;
+    for(; i < len; i += 1){
+      exUl.append(liEl);
+    };
 
-// 함수 세팅
-var cardInsertFn = function(n){
-  var menuList = coffee_list[n];
-  
-  var liN =  exLi.eq(n);
-  var imgSpace = liN.children(".img_space");
-  var imgText = imgSpace.find("span");
-  var title = liN.find("dt");
-  var con = liN.find(".data_con");
-  var linkData = liN.find(".link").children("a");
+    var exLi = exUl.children("li");
 
-  imgSpace.css({backgroundImage: menuList.image});
-  imgText.text(menuList.image_alt);
-  title.text(menuList.title);
-  con.text(menuList.content);
-  linkData.attr({href: menuList.link });
-};
+    // 함수 세팅
+    var cardInsertFn = function(n){
+      var menuList = coffee_list[n];
+      
+      var liN =  exLi.eq(n);
+      var imgSpace = liN.children(".img_space");
+      var imgText = imgSpace.find("span");
+      var title = liN.find("dt");
+      var con = liN.find(".data_con");
+      var linkData = liN.find(".link").children("a");
 
-for(i=0; i < len ; i += 1){
-  cardInsertFn(i);
-};
+      imgSpace.css({backgroundImage: menuList.image});
+      imgText.text(menuList.image_alt);
+      title.text(menuList.title);
+      con.text(menuList.content);
+      linkData.attr({href: menuList.link });
+    };
 
+    for(i=0; i < len ; i += 1){
+      cardInsertFn(i);
+    };
 
+  }); // $.ajax()
 })(jQuery);
