038085046d7614cff7b42c92cab7e277f8835259
xidoweb <xidodlab@gmail.com>
2021-10-10 00:32:22 +0900

    jQuery - position 기능 및 fade처리를 이용한 슬라이드 기능

--- b_step_05/js/src/b_02_slide_position_indicator.js
+++ b_step_05/js/src/b_02_slide_position_indicator.js
@@ -27,55 +27,34 @@ var slidePart = slideSet.find('.slide');
 var slideWrap = slidePart.find('.slide_wrapper');
 var slideDiv  = slideWrap.children('div');
 
-var i = 0;
+var i = 0, j=i;
 var timed = 500;
 
 // 함수
+var indiCheckFn = function(){
+  indiLi.eq(i).addClass('action');
+  indiLi.eq(i).siblings().removeClass('action');
+};
 
+var slideDivFn = function(){
+  // slideDiv순번에 맞는 요소를 먼저 나타나게
+  slideDiv.eq(i).stop().show();
+  slideDiv.eq(j).stop().fadeOut(timed/2, function(){
+    slideDiv.eq(i).addClass('action');
+    slideDiv.eq(j).removeClass('action');
+    j = i;
+  });
+};
 
 // 선행 기능
 slideDiv.eq(i).show();
-
-
-// 이벤트
-/* // step 1 -------------------------------
-  indiLink.on('click', function(e){
-    e.preventDefault();
-
-    i = $(this).parent().index();
-    slideDiv.eq(i).addClass('action');
-    slideDiv.eq(i).siblings().removeClass('action');
-    indiLi.eq(i).addClass('action');
-    indiLi.eq(i).siblings().removeClass('action');
-  });
-*/
 
 // step 2 -------------------------------
 indiLink.on('click', function(e){
   e.preventDefault();
   i = $(this).parent().index();
-  indiLi.eq(i).addClass('action');
-  indiLi.eq(i).siblings().removeClass('action');
-
-  // slideDiv순번에 맞는 요소를 먼저 나타나게
-  slideDiv.eq(i).show(function(){
-    // 이전에 있던 .action이었던 요소를 부드럽게 사라지게 하겠다.
-    var j=0, classCheck;
-    for(; j <= slideDiv.length ; j+=1){
-      classCheck = slideDiv.eq(j).hasClass('action'); // hasClass로 찾은 class는 true를 도출 시킨다.
-      if(classCheck === true){
-        console.log( j );
-        break;
-      }
-    }
-
-    slideDiv.eq(j).fadeOut(timed/2, function(){
-      slideDiv.eq(i).addClass('action');
-      slideDiv.eq(j).removeClass('action');
-      // slideDiv.eq(i).siblings().removeClass('action');
-    });
-
-  });
+  indiCheckFn();
+  slideDivFn();
 });
 
 
