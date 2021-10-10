bd418c2dac244c53b011a8e9bed467bb3f57d519
xidoweb <xidodlab@gmail.com>
2021-10-10 23:01:04 +0900

    jQuery - 다음 및 이전버튼 클릭으로 무한 이동(append, prepend) 및 권한부여

--- b_step_05/js/src/b_03_slide_hofizontal_btn.js
+++ b_step_05/js/src/b_03_slide_hofizontal_btn.js
@@ -10,15 +10,61 @@
  */
 
 // 변수 ======================================
+var slideSet = $('.slide_set');
+var btnArea = slideSet.find('.slide_btn');
+var nextBtn = btnArea.find('.next');
+var prevBtn = btnArea.find('.prev');
 
+var horizontal = slideSet.find('.horizontal_slide');
+var horizonDiv = horizontal.children('div'); // 01-02-03-04-05
+
+var permission = true; // 출입 패스권한(승인)
 
 // 함수 ======================================
-
-
 // 기능 수행 ======================================
 
 
 // 이벤트 ======================================
+// next버튼 클릭시 수행
+nextBtn.on('click', function(e){
+  e.preventDefault();
+  // nextBtn.hide();
 
+  if(permission){
+
+    permission =false;  // 출입 패스권한(승인취소)
+    
+    horizontal.stop().animate({marginLeft:-100+'%'}, function(){
+      var divFirst =  horizonDiv.eq(0);
+      // divFirst.appendTo(horizontal);
+      horizontal.append( divFirst ); // 02-03-04-05-01
+      horizontal.css({marginLeft:0});
+      horizonDiv = horizontal.children('div');  // 02-03-04-05-01
+    
+      // nextBtn.show();
+      permission = true;
+    });
+
+  }// if(permission)
+});// nextBtn.on('click')
+
+// prev버튼 클릭시 수행
+prevBtn.on('click', function(e){
+  e.preventDefault();
+  // prevBtn.hide();
+  if(permission){
+    permission = false; 
+
+    var divLast = horizonDiv.eq(-1);
+    horizontal.prepend(divLast);
+    horizontal.css({marginLeft:-100 + '%'});
+    horizontal.stop().animate({marginLeft: 0}, function(){
+      horizonDiv = horizontal.children('div');
+      prevBtn.show();
+      permission = true;
+    });
+
+  }// if(permission)
+}); // prevBtn.on('click')
 
 })(jQuery);
